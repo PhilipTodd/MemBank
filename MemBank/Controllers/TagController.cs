ad9e5822ef15827caaa7a068463c8e3a5ce5dc69
Philip Todd <philip.todd@gmail.com>
2018-10-17 21:38:27 +1100

    Add project files.

---
+++ MemBank/Controllers/TagController.cs
@@ -0,0 +1,57 @@
+﻿using MemBank.DAL.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+//using static MemBankCoreAngular.Global.Constants;
+using MemBank.Models;
+
+namespace MemBankCoreAngular.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TagController : ControllerBase
+    {
+        private TagRepository _repository;
+
+        public TagController(TagRepository repository)
+        {
+            this._repository = repository;
+        }
+
+        //[HttpGet("[action]")]
+        //public IEnumerable<Tag> GetForParentType (int parentTypeId)
+        //{
+
+        //}
+
+        [HttpGet]
+        public ActionResult<List<Tag>> GetAll()
+        {
+            return this._repository.GetAll().ToList();
+        }
+
+        [HttpGet("{id}", Name = "GetById")]
+        public ActionResult<Tag> GetById(int id)
+        {
+            var data = this._repository.GetById(id);
+            if (data == null)
+            {
+                return NotFound();
+            }
+            return data;
+        }
+
+        [HttpGet("GetTagCloudData/{parentTypeId}")]
+        public ActionResult<List<TagCloudData>> GetTagCloudData(int parentTypeId)
+        {
+            var data = this._repository.GetTagCloudData(parentTypeId);
+            if (data == null)
+            {
+                return NotFound();
+            }
+            return data.ToList();
+        }
+    }
+}
---
+++ MemBank/DAL/Repository/TagRepository.cs
@@ -0,0 +1,63 @@
+﻿using MemBank.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MemBank.DAL.Repository
+{
+    public class TagRepository : BaseRepository
+    {
+        public TagRepository(MemBankContext context)
+        {
+            this._context = context;
+        }
+
+        public IEnumerable<Tag> GetAll()
+        {
+            return this._context.Tag;
+        }
+
+        public IEnumerable<Tag> GetTagsForParentType(int parentTypeId)
+        {
+            return this._context.Tag.Where(tag => tag.ParentTypeId == parentTypeId).OrderByDescending(tag => tag.Text);
+        }
+
+        public IEnumerable<Tag> GetTagsForParent(int parentId)
+        {
+            return this._context.Tag.Where(tag => tag.ParentId == parentId).OrderByDescending(tag => tag.Text);
+        }
+
+        public IEnumerable<TagCloudData> GetTagCloudData(int parentTypeId)
+        {
+            return this._context.Tag.Where(tag => tag.ParentTypeId == parentTypeId)
+                .GroupBy(tag => tag.Text)
+                .Select(t => new TagCloudData
+                {
+                    Weight = t.Count(),
+                    Text = t.Key,
+                });
+        }
+
+        public Tag GetById(int id)
+        {
+            var item = _context.Tag.Find(id);
+            if (item == null)
+            {
+                return null;
+            }
+            return item;
+        }
+
+        public void Insert(Tag tag)
+        {
+            this._context.Tag.Add(tag);
+        }
+
+        public void Delete(Tag tag)
+        {
+            this._context.Tag.Remove(tag);
+        }
+
+    }
+}
